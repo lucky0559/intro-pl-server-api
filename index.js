57a2c2773a62e1c821f88d59f6d3ee5415bce875
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2021-07-29 12:18:09 +0800

    coms

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const authRoutes = require('./src/routes/authRoutes');
+const requireAuth = require('./src/middleware/requireAuth');
 
 
 const app = express();
 let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(authRoutes);
 
 
 const mongoUri = 'mongodb+srv://intro-pl:123@intro-pl@cluster0.kjlec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -20,8 +23,8 @@ mongoose.connection.on('error', (err) => {
 });
 
 
-app.get('/',(res, req) => {
-    res.send('Hello World');
+app.get('/',requireAuth, (req, res) => {
+    res.send(`Your email is: ${req.user.email}`);
 });
 
 app.listen(port, () => {
